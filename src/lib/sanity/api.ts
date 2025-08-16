278c7598bed8371c95b1629a7ce2c42ebec4ebbb
Aaron Emery <aaron.emery@sanity.io>
2025-08-16 13:51:39 -0700

    feat: generated typegen file

---
+++ src/lib/sanity/api.ts
@@ -0,0 +1,19 @@
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage);
+  }
+  return v;
+}
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production';
+
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+);
+
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? 'v2025-08-16';
+
+export const studioUrl =
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_URL ?? 'http://localhost:3333';
